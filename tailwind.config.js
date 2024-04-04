8564a9a8eed526b4a6f982a39987634eda2a7d80
Ali <a.komijani@wallex.net>
2024-04-04 15:31:01 +0330

    add icon package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import { addDynamicIconSelectors } from "@iconify/tailwind";
 export default {
   content: ["./index.html", "./src/**/*.{js,}", "./main.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()],
 };
