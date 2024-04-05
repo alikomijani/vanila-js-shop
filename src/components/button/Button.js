3a5eb75c90562f4cbd299cfda9df589ce1861299
Ali <a.komijani@wallex.net>
2024-04-05 18:01:03 +0330

    fix:fix button style

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,10 +1,11 @@
 import { El } from "@/utils";
-export function Button({ text, ...rest }) {
+export function Button({ text, className = "", ...rest }) {
   return El({
     element: "button",
     innerText: text,
     className:
-      "rounded-md text-center block w-full p-2 bg-red-500 text-white hover:bg-red-600",
+      "rounded-md text-center block w-full p-2 bg-red-500 text-white hover:bg-red-600 " +
+      className,
     ...rest,
   });
 }
