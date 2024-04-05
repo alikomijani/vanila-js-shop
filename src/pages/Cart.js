9aed50c4bc7e73409004c39903c1bdeaebd296ad
Ali <a.komijani@wallex.net>
2024-04-05 16:48:25 +0330

    feat:add cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,9 @@
+import Cart from "@/templates/Cart";
 import { El } from "@/utils";
-
+import { Layout } from "@/layout/layout";
 export default function CartPage() {
   return El({
     element: "div",
-    innerText: "cartPage",
+    children: [Layout(Cart())],
   });
 }
---
+++ src/templates/Cart.js
@@ -0,0 +1,8 @@
+import { El } from "@/utils";
+
+export default function Cart() {
+  return El({
+    element: "div",
+    innerText: "cart",
+  });
+}
