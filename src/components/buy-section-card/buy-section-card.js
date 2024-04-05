3b8e0771ddaf85a7ec859f7e03b7583a37308e28
Ali <a.komijani@wallex.net>
2024-04-05 10:25:18 +0330

    feat: add product buy-section-card component

---
+++ src/components/buy-section-card/buy-section-card.js
@@ -0,0 +1,26 @@
+import { El } from "@/utils";
+import { Button } from "../button/Button";
+
+export function BuySectionCard({ product }) {
+  return El({
+    element: "div",
+    className: "border rounded-md bg-gray-100 p-2 w-1/4",
+    children: [
+      El({
+        element: "div",
+        className: "flex justify-between my-2",
+        children: [
+          El({
+            element: "p",
+            innerText: "قیمت",
+          }),
+          El({
+            element: "p",
+            innerText: product.price,
+          }),
+        ],
+      }),
+      Button({ text: "افزودن به سبد خرید" }),
+    ],
+  });
+}
