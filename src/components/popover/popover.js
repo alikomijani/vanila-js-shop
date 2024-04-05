33547cdaab171658aff6c1ea88cbc9a07bb75be2
Ali <a.komijani@wallex.net>
2024-04-05 18:01:18 +0330

    fix: fix popover

--- src/components/popover/popover.js
+++ src/components/popover/popover.js
@@ -3,12 +3,12 @@ import { El } from "@/utils";
 export function Popover({ parent, child }) {
   return El({
     element: "div",
-    className: "relative group",
+    className: "relative group block",
     children: [
       parent,
       El({
         element: "div",
-        className: "invisible group-hover:visible absolute bottom-0",
+        className: "invisible group-hover:visible absolute top-[40px] left-0",
         children: [child],
       }),
     ],
